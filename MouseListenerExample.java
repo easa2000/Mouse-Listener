67c2d75e291d0e5d7fd343a33c7d6ef93117a5fb
Mahudoom <66937573+easa2000@users.noreply.github.com>
2021-11-10 22:39:57 +0530

    Create MouseListenerExample.java

---
+++ MouseListenerExample.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseListenerExample extends Frame implements MouseListener {
+    Label l;
+
+    MouseListenerExample() {
+        addMouseListener(this);
+
+        l = new Label();
+        l.setBounds(20,50,100,20);
+        add(l);
+        setSize(300,300);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        l.setText("Mouse Clicked");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        l.setText("Mouse Pressed");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        l.setText("Mouse Released");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        l.setText("Mouse Entered");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        l.setText("Mouse Exited");
+    }
+
+    public static void main(String[] args) {
+        new MouseListenerExample();
+    }
+}
